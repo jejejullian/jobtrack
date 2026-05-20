491bc4fa1647f125afecc26a1c157ffca3c8afea
jeje <juliannurfadzlin659@gmail.com>
2026-05-20 10:04:49 +0700

    refactor: authmiddleware function

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,20 +1,19 @@
-import jwt from "jsonwebtoken";
-import AppError from "../utils/AppError.js";
+import { promisify } from "util";
+const verifyToken = promisify(jwt.verify);
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+const authenticateToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    throw new AppError("Authorization failed. No access token.", 401);
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      console.error(err);
-      throw new AppError("Could not verify token", 403);
+    if (!token) {
+      throw new AppError("Authorization failed. No access token.", 401);
     }
+
+    const user = await verifyToken(token, process.env.JWT_SECRET);
     req.user = user;
     next();
-  });
+  } catch (err) {
+    next(err);
+  }
 };

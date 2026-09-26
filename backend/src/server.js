957ad6c766f670c1ab5e2ba6d770b3ddc745492e
jejejullian <juliannurfadzlin659@gmail.com>
2026-09-26 14:14:49 +0700

    chore: remove legacy frontend and backend folders

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-
-const PORT = process.env.PORT || 3000;
-
-// start server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
--- backend/src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// wrap async route handler
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next)
-}
-
-export default asyncHandler
